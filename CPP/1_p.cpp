014706ce994bcae319d21b38237ae3e981ff0104
ZishanManna <139680050+ZishanManna@users.noreply.github.com>
2024-10-01 10:13:44 +0530

    Update 1_p.cpp

--- CPP/1_p.cpp
+++ CPP/1_p.cpp
@@ -8,10 +8,6 @@ class Person {
 public:
     Person(string name, int age) : name(name), age(age) {}
 
-    void print_info() {
-        cout << "Name: " << name << ", Age: " << age << endl;
-    }
-
 private:
     string name;  // Name attribute
     int age;      // Age attribute
@@ -20,11 +16,5 @@ private:
 int main() {
     // Creating instances of the Person class
     Person person1("Alice", 30);
-    Person person2("Bob", 45);
-
-    // Printing information for both instances
-    person1.print_info();
-    person2.print_info();
-
     return 0;
 }
