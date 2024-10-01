37612efd5b05a216d6a29e4e4fea40ba66482c8b
ZishanManna <139680050+ZishanManna@users.noreply.github.com>
2024-10-01 10:17:06 +0530

    Update 2_p.cpp

--- CPP/2_p.cpp
+++ CPP/2_p.cpp
@@ -1,33 +1,33 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
-// Define the Person class with name and age attributes
+// Define the Person class with public name and age attributes
 class Person {
 public:
     // Constructor to initialize name and age
-    Person(string name, int age) : name(name), age(age) {}
+    Person(string person_name, int person_age) {
+        name = person_name;
+        age = person_age;
+    }
 
     // Function to print information about the person
     void print_info() {
         cout << "Name: " << name << ", Age: " << age << endl;
     }
-
-private:
-    string name;  // Name attribute
-    int age;      // Age attribute
+    //Public attributes
+    string name;
+    int age;
 };
 
 int main() {
-    // Creating instances of the Person class
+    // Create two instances of the Person class
     Person person1("Alice", 30);
     Person person2("Bob", 45);
 
-    // Printing information for both instances
+    // Call print_info() on each instance
     person1.print_info();
     person2.print_info();
 
     return 0;
 }
-
